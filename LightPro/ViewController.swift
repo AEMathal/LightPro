9b35a823860d3c1e0adb76d307fc60ad1e7bd5ea
Abdullah Eid <i2elli67@gmail.com>
2023-01-29 14:47:56 +0300

    Initial Commit

---
+++ LightPro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LightPro
+//
+//  Created by Abdullah Eid on 07/07/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
